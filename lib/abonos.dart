66b0304736ceac546cbc9e28b746c06b87390eab
gabrielguimaraes05 <gabriel.prguimaraes@gmail.com>
2019-11-23 01:14:14 -0300

    Alteracoes

--- lib/abonos.dart
+++ lib/abonos.dart
@@ -22,8 +22,9 @@ class _AbonosState extends State<Abonos> {
     super.initState();
   }
 
-    Future<List<Abono>> _buscaAbonos() async {
-    final url = "https://bate-ponto-backend.herokuapp.com/abonos?buscarTudo=true";
+  Future<List<Abono>> _buscaAbonos() async {
+    final url =
+        "https://bate-ponto-backend.herokuapp.com/abonos?buscarTudo=true";
 
     Map<String, String> headers = {
       'Authorization': await getToken(),
@@ -44,33 +45,33 @@ class _AbonosState extends State<Abonos> {
         pageTitle: Abonos.titulo,
         body: SafeArea(
           child: FutureBuilder(
-            future: _buscaAbonos(),
-            builder:
-              (BuildContext context, AsyncSnapshot<List<Abono>> snapshot) {
-            if (snapshot == null || snapshot.hasError) {
-              return Center(
-                child: Text("Não foi possível buscar os abonos"),
-              );
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              List<Abono> abonos = snapshot.data;
-              Widget lista = _buildListaAbonos(abonos);
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 16.0),
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: lista,
+              future: _buscaAbonos(),
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<Abono>> snapshot) {
+                if (snapshot == null || snapshot.hasError) {
+                  return Center(
+                    child: Text("Não foi possível buscar os abonos"),
+                  );
+                } else if (snapshot.connectionState == ConnectionState.done) {
+                  List<Abono> abonos = snapshot.data;
+                  Widget lista = _buildListaAbonos(abonos);
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 16.0),
+                    child: Column(
+                      children: <Widget>[
+                        Expanded(
+                          child: lista,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
         ));
   }
 }
@@ -78,12 +79,13 @@ class _AbonosState extends State<Abonos> {
 Widget _buildListaAbonos(List<Abono> abonos) {
   return Center(
     child: Container(
+      alignment: Alignment.centerRight,
       constraints: BoxConstraints(
-        maxWidth: 350,
+        maxWidth: 800,
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-        child:ListView.builder(
+        child: ListView.builder(
           itemBuilder: (context, index) {
             Abono abono = abonos[index];
             return Padding(
@@ -92,38 +94,80 @@ Widget _buildListaAbonos(List<Abono> abonos) {
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                    "${abono.nome}",
-                                    style: Theme.of(context).textTheme.title,
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  Text(
-                                    "Descrição: ${abono.motivo}",
-                                    style: Theme.of(context).textTheme.body1,
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  Text(
-                                    "Data Solicitada:",
-                                    style: Theme.of(context).textTheme.body1,
-                                  ),
-                              ]
-                            )
+                          Text(
+                            "${abono.nome}",
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.cloud_download),
+                            onPressed: () {},
                           )
-                        ]
+                        ],
+                      ),
+                      Row(children: <Widget>[
+                        Text(
+                          "Descrição: ${abono.motivo}",
+                          style: Theme.of(context).textTheme.body1,
+                        ),
+                      ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                "Data solicitada: ${abono.dataAbonada}",
+                                style: Theme.of(context).textTheme.body1,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            
+                            children: [
+                            FlatButton(
+                              color: Colors.green,
+                              textColor: Colors.white,
+                              disabledColor: Colors.grey,
+                              disabledTextColor: Colors.black,
+                              padding: EdgeInsets.all(2.0),
+                              splashColor: Colors.blueAccent,
+                              onPressed: () {},
+                              child: Text(
+                                "Aprovado",
+                                style: TextStyle(
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.all(10),),
+                            FlatButton(
+                              color: Colors.red,
+                              textColor: Colors.white,
+                              disabledColor: Colors.grey,
+                              disabledTextColor: Colors.black,
+                              padding: EdgeInsets.all(2.0),
+                              splashColor: Colors.blueAccent,
+                              onPressed: () {},
+                              child: Text(
+                                "Recusado",
+                                style: TextStyle(
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ])
+                        ],
                       )
-                    ]
-                  )
-                )
-              )
+                    ],
+                  ),
+                ),
+              ),
             );
           },
           itemCount: abonos.length,
